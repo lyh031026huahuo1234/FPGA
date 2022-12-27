5dc46de748a9b6c0a2f98d666d9cafa90616ae1c
lyh031026huahuo1234 <112098685+lyh031026huahuo1234@users.noreply.github.com>
2022-12-27 09:48:42 +0800

    merge version3's RegFile.v to version4

--- Project_version4/RegFile.v
+++ Project_version4/RegFile.v
@@ -15,42 +15,49 @@ module RegFile(
 
     //在这个module中管理了8个寄存器,每个寄存器的数据为16位
     reg [15:0] register[7:0];
-    reg [15:0] R0;
-    reg [15:0] R1;
-    reg [15:0] R2;
-    reg [15:0] R3;
-    reg [15:0] R4;
-    reg [15:0] R5;
-    reg [15:0] R6;
-    reg [15:0] R7;
+    wire [15:0] R0;
+    wire [15:0] R1;
+    wire [15:0] R2;
+    wire [15:0] R3;
+    wire [15:0] R4;
+    wire [15:0] R5;
+    wire [15:0] R6;
+    wire [15:0] R7;
     
-
-    integer i;
     initial begin
-        for(i = 0; i < 16; i = i + 1) begin
-            register[i] <= 0;
-        end
+        $readmemb("test/reg.txt", register);    //读取测试文档中的指令
     end
+    // integer i;
+    // initial begin
+    //     for(i = 0; i < 16; i = i + 1) begin
+    //         register[i] <= 0;
+    //     end
+    // end
 
     // 读取源寄存器
     assign SR1_value = register[SR1];
     assign SR2_value = register[SR2];
 
-    // 写入目的寄存器
-    always @(posedge clk) begin
-        R0 = register[0];
-        R1 = register[1];
-        R2 = register[2];
-        R3 = register[3];
-        R4 = register[4];
-        R5 = register[5];
-        R6 = register[6];
-        R7 = register[7];
+    assign        R0 = register[0];
+    assign    R1 = register[1];
+    assign    R2 = register[2];
+     assign   R3 = register[3];
+     assign   R4 = register[4];
+     assign   R5 = register[5];
+     assign   R6 = register[6];
+     assign   R7 = register[7];
 
+    // 写入目的寄存器
+    always @(negedge clk) begin
+       
         //写使能信号
-        if (RegWE) begin
+        if (RegWE==1'b1) begin
             register[DR] = DR_value;
         end
+
+
+
+
     end
 
 endmodule
